ab1d8f266d3d3a204b6e266def7f36b1fb75af73
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-04-19 03:08:19 +0200

    Implement reset and toggle player move function

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const boxes = document.querySelectorAll('.box');
-let toggleMove = false;
+const resetBoard = document.querySelector('.reset');
+
+let currentPlayer = 'X'; // Initialize current player as X
+
+// Function to toggle player turn
+function togglePlayer() {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X'; // Toggle between X and O
+}
+
+// Function to reset the game
+function resetGame() {
+    boxes.forEach(box => {
+        box.textContent = "";
+    })
+
+    currentPlayer = "";
+}
+
+resetBoard.addEventListener('click', () => {
+    resetGame();
+})
 
 boxes.forEach(box => {
     box.addEventListener('click', () => {
-        if (toggleMove === false) {
-            box.textContent = 'X';
-            toggleMove = !toggleMove;
-        } else {
-            box.textContent = 'O';
-            toggleMove = !toggleMove
+        if (!box.textContent) {
+            box.textContent = currentPlayer;
+            togglePlayer();
         }
-
-
     })
+
 })
