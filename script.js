a6872a02551c1a1a6e883b86593fa38fa36b1c0d
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-04-19 15:46:09 +0200

    Implement checkwin and check draw function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const boxes = document.querySelectorAll('.box');
 const resetBoard = document.querySelector('.reset');
+const result = document.querySelector('.result');
+let winnerFound = false;
 
 let currentPlayer = 'X'; // Initialize current player as X
 
@@ -17,15 +19,65 @@ function resetGame() {
     currentPlayer = "";
 }
 
+
+// Function to check if the current player has won
+function checkWin() {
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let combo of winningCombos) {
+        const [a, b, c] = combo;
+        const boxA = document.querySelector(`.box[data-index="${a}"]`).textContent;
+        const boxB = document.querySelector(`.box[data-index="${b}"]`).textContent;
+        const boxC = document.querySelector(`.box[data-index="${c}"]`).textContent;
+
+        if (boxA && boxA === boxB && boxB === boxC) {
+            winnerFound = true;
+            return boxA;
+        }
+    }
+    return null;
+}
+
+function checkDraw() {
+    for (let box of boxes) {
+        if (!box.textContent) {
+            return false
+        }
+    }
+
+    return !checkWin();
+}
+
 resetBoard.addEventListener('click', () => {
+    winnerFound = false;
+    result.textContent = '';
     resetGame();
 })
 
 boxes.forEach(box => {
     box.addEventListener('click', () => {
-        if (!box.textContent) {
-            box.textContent = currentPlayer;
-            togglePlayer();
+        if (!winnerFound) {
+            if (!box.textContent) {
+                box.textContent = currentPlayer;
+                const winner = checkWin();
+                if (winner) {
+                    result.textContent = `${winner} is winner`;
+                    winnerFound = true;
+                } else if (checkDraw()) {
+                    result.textContent = "It's a draw";
+                    winnerFound = true;
+                }
+                togglePlayer();
+            }
         }
     })
 
